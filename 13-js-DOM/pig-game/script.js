ef85781d64b1468c628570871132d47224e02299
Tae Woo Kim <taewookim02@gmail.com>
2022-05-28 01:58:15 +0800

    advanced DOM

--- 13-js-DOM/pig-game/script.js
+++ 13-js-DOM/pig-game/script.js
@@ -1,18 +1,18 @@
-"use strict";
+'use strict';
 
 // Selecting elements
-const player0El = document.querySelector(".player--0");
-const player1El = document.querySelector(".player--1");
-const score0El = document.querySelector("#score--0");
-const score1El = document.getElementById("score--1");
-const current0El = document.getElementById("current--0");
-const current1El = document.getElementById("current--1");
-const currentScoreEl = document.querySelector(".current-score");
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
+const score0El = document.querySelector('#score--0');
+const score1El = document.getElementById('score--1');
+const current0El = document.getElementById('current--0');
+const current1El = document.getElementById('current--1');
+const currentScoreEl = document.querySelector('.current-score');
 
-const diceEl = document.querySelector(".dice");
-const btnNew = document.querySelector(".btn--new");
-const btnRoll = document.querySelector(".btn--roll");
-const btnHold = document.querySelector(".btn--hold");
+const diceEl = document.querySelector('.dice');
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
 // Starting conditions
 // score0El.textContent = 0;
 // score1El.textContent = 0;
@@ -37,15 +37,15 @@ const init = function () {
 
   // active player to 1
   activePlayer = 0;
-  player0El.classList.add("player--active");
-  player1El.classList.remove("player--active");
+  player0El.classList.add('player--active');
+  player1El.classList.remove('player--active');
 
   // dice display hidden
-  diceEl.classList.add("hidden");
+  diceEl.classList.add('hidden');
 
   // remove player--winner
-  player0El.classList.remove("player--winner");
-  player1El.classList.remove("player--winner");
+  player0El.classList.remove('player--winner');
+  player1El.classList.remove('player--winner');
 
   // playing to true
   playing = true;
@@ -55,18 +55,18 @@ const switchPlayer = function () {
   document.getElementById(`current--${activePlayer}`).textContent = 0;
   currentScore = 0;
   activePlayer = activePlayer === 0 ? 1 : 0;
-  player0El.classList.toggle("player--active");
-  player1El.classList.toggle("player--active");
+  player0El.classList.toggle('player--active');
+  player1El.classList.toggle('player--active');
 };
 
 // Rolling dice functionality
-btnRoll.addEventListener("click", function () {
+btnRoll.addEventListener('click', function () {
   if (playing) {
     // 1. Generating a random dice roll
     const dice = Math.trunc(Math.random() * 6) + 1;
 
     // 2. Display dice
-    diceEl.classList.remove("hidden");
+    diceEl.classList.remove('hidden');
     diceEl.src = `dice-${dice}.png`;
 
     // 3. Check for rolled 1:
@@ -83,7 +83,7 @@ btnRoll.addEventListener("click", function () {
   }
 });
 
-btnHold.addEventListener("click", function () {
+btnHold.addEventListener('click', function () {
   if (playing) {
     // 1. Add current score to active player's score
     scores[activePlayer] += currentScore;
@@ -92,16 +92,17 @@ btnHold.addEventListener("click", function () {
 
     // 2. Check if player's score is >= 100
     if (scores[activePlayer] >= 100) {
+      0;
       // Finish the game
       playing = false;
-      diceEl.classList.add("hidden");
+      diceEl.classList.add('hidden');
 
       document
         .querySelector(`.player--${activePlayer}`)
-        .classList.add("player--winner");
+        .classList.add('player--winner');
       document
         .querySelector(`.player--${activePlayer}`)
-        .classList.remove("player--active");
+        .classList.remove('player--active');
     }
 
     // Switch to the next player
@@ -111,6 +112,6 @@ btnHold.addEventListener("click", function () {
   }
 });
 
-btnNew.addEventListener("click", init);
+btnNew.addEventListener('click', init);
 
 init();
