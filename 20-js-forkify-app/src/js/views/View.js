d9bbcd3112289d4730139484e4f478a8b0ff2c13
Tae Woo Kim <taewookim02@gmail.com>
2022-06-24 09:56:46 +0800

    feat: getting error while adding data-update-to feature in view.js

--- 20-js-forkify-app/src/js/views/View.js
+++ 20-js-forkify-app/src/js/views/View.js
@@ -22,6 +22,28 @@ export default class View {
     const newMarkup = this._generateMarkup();
 
     const newDOM = document.createRange().createContextualFragment(newMarkup);
+    const newElements = Array.from(newDOM.querySelectorAll('*'));
+    const curElements = Array.from(this._parentElement.querySelectorAll('*'));
+
+    newElements.forEach((newEl, i) => {
+      const curEl = curElements[i];
+      console.log(curEl, newEl.isEqualNode(curEl));
+
+      // Updates changed TEXT
+      if (
+        !newEl.isEqualNode(curEl) &&
+        newEl.firstChild?.nodeValue.trim() !== ''
+      ) {
+        // console.log('💥', newEl.firstChild.nodeValue.trim());
+        curEl.textContent = newEl.textContent;
+      }
+
+      // Updates changed ATTRIBUTES // bug!
+      if (!newEl.isEqualNode(curEl))
+        Array.from(newEl.attributes).forEach(attr =>
+          curEl.setAttribute(attr.name, attr.name)
+        );
+    });
   }
 
   _clear() {
